8c9a141703030e4e24572ae5969e36b11c75b5e2
Thang Hoang <thanghoang064@gmail.com>
2018-05-06 18:36:24 +0700

    Custom layout list order

--- app/src/main/java/com/example/thuan/hotel/Model/Oder.java
+++ app/src/main/java/com/example/thuan/hotel/Model/Oder.java
@@ -1,8 +1,27 @@
 package com.example.thuan.hotel.Model;
 
 public class Oder {
+    private String name_hotel;
+    private String address_hotel;
     private  String name;
     private  String email;
+
+    public String getName_hotel() {
+        return name_hotel;
+    }
+
+    public void setName_hotel(String name_hotel) {
+        this.name_hotel = name_hotel;
+    }
+
+    public String getAddress_hotel() {
+        return address_hotel;
+    }
+
+    public void setAddress_hotel(String address_hotel) {
+        this.address_hotel = address_hotel;
+    }
+
     private String phone;
     private String DateStartOrder;
     private String DateEndOrder;
@@ -12,9 +31,10 @@ public class Oder {
     public Oder() {
     }
 
-    public Oder(String name, String email, String phone, String dateStartOrder, String dateEndOrder, int roomOrder, float totalMoney) {
+    public Oder(String name_hotel, String address_hotel, String name,  String phone, String dateStartOrder, String dateEndOrder, int roomOrder, float totalMoney) {
+        this.name_hotel = name_hotel;
+        this.address_hotel = address_hotel;
         this.name = name;
-        this.email = email;
         this.phone = phone;
         DateStartOrder = dateStartOrder;
         DateEndOrder = dateEndOrder;
